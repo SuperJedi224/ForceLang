e2e61d8bfbfe444715dd15efca17b3498ff83f20
Johnathan Waugh <superjedi224@gmail.com>
2016-12-12 23:47:20 -0500

    Add files via upload

--- src/lang/FString.java
+++ src/lang/FString.java
@@ -9,10 +9,17 @@ public class FString extends FObj {
 	public FString(String s){
 		val=s;
 		this.set("len",new FNum(s.length()));
+		this.set("repeat",new Function(a->{
+			FObj o=ForceLang.parse(a);
+			StringBuilder f=new StringBuilder();
+			try{int l=((FNum)o).intValue();
+			for(;l-->0;)f.append(val);
+			return new FString(f.toString());}catch(Exception e){throw new IllegalArgumentException("Expected Number.");}
+		}));
 		this.set("charAt",new Function(a->{
 			FObj o=ForceLang.parse(a);
 			try{return new FString(""+val.charAt(((FNum)o).intValue()));}catch(Exception e){
-				throw new IllegalArgumentException("Expected Number.");
+				throw new IllegalArgumentException("Input given is non-numeric or out of bounds.");
 			}	
 		}));
 		this.setImmutable();
